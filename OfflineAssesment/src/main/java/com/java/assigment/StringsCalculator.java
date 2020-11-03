9f1c89837973e75a326bcd202fd165077bd84998
Vamsi Krishna <vamsibk1997@gmail.com>
2020-11-03 20:20:49 +0530

    Step 3 of Assignment
    Results:
     Test 1, String is:empty, the result is:0
     Test 2, String is: 1, the result is:1
     Test 3, String is: 1,2, the result is:3
     Test 4, String is: 1,2,3 the result is:6
     Test 5, String is: 1,,3 the result is:4
     Test 6, String is: 1, the result is:1
     Test 7, String is: ,3 the result is:3
     Test 8, String is: 1\n2,3 the result is:6
     Test 9, String is: 1\n2,3\n4,5,6\n7,9 the result is:37
     Test 10,String is: 10\n,3\n5,6 the result is:24
     Test 11,String is: 10\n,3\n5,\n the result is:18

--- OfflineAssesment/src/main/java/com/java/assigment/StringsCalculator.java
+++ OfflineAssesment/src/main/java/com/java/assigment/StringsCalculator.java
@@ -4,6 +4,14 @@ public class StringsCalculator {
 
 	public int returnValue(String str)//method parses and returns the value of the current string
 	{
+		if(!str.isEmpty() && str.indexOf('n') != -1)//this snippet will handle the new line characters if any in the current string
+		{
+			int newCharIndex = str.indexOf('n');
+			String num1 = str.substring(0,newCharIndex-1);
+			String num2 = str.substring(newCharIndex+1);
+
+			return returnValue(num1)+returnValue(num2);
+		}
 		return str.isEmpty()?0:Integer.parseInt(str);
 	}
 
--- OfflineAssesment/src/test/java/com/java/assignment/tests/TestStringNumbers.java
+++ OfflineAssesment/src/test/java/com/java/assignment/tests/TestStringNumbers.java
@@ -5,13 +5,17 @@ import com.java.assigment.StringsCalculator;
 public class TestStringNumbers {
 
 	public static void main(String[] args) {
-		StringsCalculator str = new StringsCalculator();
-		System.out.println(" Test 1, String is:empty, the result is:" + str.add(""));
-		System.out.println(" Test 2, String is: 1, the result is:" + str.add("1"));
-		System.out.println(" Test 3, String is: 1,2, the result is:" + str.add("1,2"));
-		System.out.println(" Test 4, String is: 1,2,3 the result is:" + str.add("1,2,3"));
-		System.out.println(" Test 5, String is: 1,,3 the result is:" + str.add("1,,3"));
-		System.out.println(" Test 6, String is: 1, the result is:" + str.add("1,"));
-		System.out.println(" Test 7, String is: ,3 the result is:" + str.add(",3"));
+		StringsCalculator obj = new StringsCalculator();
+		System.out.println(" Test 1, String is:empty, the result is:" + obj.add(""));
+		System.out.println(" Test 2, String is: 1, the result is:" + obj.add("1"));
+		System.out.println(" Test 3, String is: 1,2, the result is:" + obj.add("1,2"));
+		System.out.println(" Test 4, String is: 1,2,3 the result is:" + obj.add("1,2,3"));
+		System.out.println(" Test 5, String is: 1,,3 the result is:" + obj.add("1,,3"));
+		System.out.println(" Test 6, String is: 1, the result is:" + obj.add("1,"));
+		System.out.println(" Test 7, String is: ,3 the result is:" + obj.add(",3"));
+		System.out.println(" Test 8, String is: 1\\n2,3 the result is:" + obj.add("1\\n2,3"));
+		System.out.println(" Test 9, String is: 1\\n2,3\\n4,5,6\\n7,9 the result is:" + obj.add("1\\n2,3\\n4,5,6\\n7,9"));
+		System.out.println(" Test 10,String is: 10\\n,3\\n5,6 the result is:" + obj.add("10\\n,3\\n5,6"));
+		System.out.println(" Test 11,String is: 10\\n,3\\n5,\\n the result is:" + obj.add("10\\n,3\\n5,\\n"));
 	}
 }
