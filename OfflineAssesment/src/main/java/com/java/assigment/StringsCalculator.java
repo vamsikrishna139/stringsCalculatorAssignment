eb67837134067845c0bb76dfd8774dc58c3f91cf
Vamsi Krishna <vamsibk1997@gmail.com>
2020-11-03 21:23:43 +0530

    Step 3 + partial Step 4 of the assignment: Made Wrong assumption about the new line Results:  Test 1, String is:empty, the result is:0  Test 2, String is: 1, the result is:1  Test 3, String is: 1,2, the result is:3  Test 4, String is: 1,2,3 the result is:6  Test 5, String is: 1,,3 the result is:4  Test 6, String is: 1, the result is:1  Test 7, String is: ,3 the result is:3  Test 8, String is: 1\n2,3 the result is:6  Test 9, String is: 1\n2,3\n4,5,6\n7,9 the result is:37  Test 10,String is: 10\n,3\n5,6 the result is:24  Test 11,String is: 10\n,3\n5,\n the result is:18  Test 12,String is: //;\n1;2 the result is:3

--- OfflineAssesment/src/main/java/com/java/assigment/StringsCalculator.java
+++ OfflineAssesment/src/main/java/com/java/assigment/StringsCalculator.java
@@ -4,13 +4,10 @@ public class StringsCalculator {
 
 	public int returnValue(String str)//method parses and returns the value of the current string
 	{
-		if(!str.isEmpty() && str.indexOf('n') != -1)//this snippet will handle the new line characters if any in the current string
+		if(!str.isEmpty() && str.contains("\n"))//this snippet will handle the new line characters if any in the current string
 		{
-			int newCharIndex = str.indexOf('n');
-			String num1 = str.substring(0,newCharIndex-1);
-			String num2 = str.substring(newCharIndex+1);
-
-			return returnValue(num1)+returnValue(num2);
+			String[] lines = str.split("\n");
+			return calculateValue(lines);
 		}
 		return str.isEmpty()?0:Integer.parseInt(str);
 	}
@@ -22,13 +19,13 @@ public class StringsCalculator {
 		{
 			return sum;
 		}
+		else if(numbers.indexOf("//") == 0)//this scenario handles the variable delimiter scenario
+		{
+			sum += variableDelimiter(numbers);
+		}
 		else if(numbers.indexOf(',') != -1)//this is for more than one number scenario
 		{
-			String[] strs = numbers.split(",");
-			for(String s:strs) 
-			{
-				sum += returnValue(s);
-			}
+			sum += calculateValue(numbers,",");
 		}
 		else//this is for single number scenario
 		{
@@ -36,5 +33,42 @@ public class StringsCalculator {
 		}
 
 		return sum;
+	}
+
+	public int calculateValue(String numbers, String delimiter) //utility method for various purposes
+	{
+		int curr = 0;
+
+		String[] strs = numbers.split(delimiter);
+		for(String s:strs) 
+		{
+			curr += returnValue(s);
+		}
+
+		return curr;
+	}
+
+	public int calculateValue(String[] numbers) //utility method for various purposes
+	{
+		int curr = 0;
+		for(String s:numbers) 
+		{
+			curr += returnValue(s);
+		}
+
+		return curr;
+	}
+
+	private int variableDelimiter(String numbers)// this figures out the delimiter and then process the numbers string accordingly
+	{
+		int curr = 0;
+
+		if(numbers.length() > 3)
+		{
+			String currDelimiter = numbers.substring(2,3);
+			String[] splitLines = numbers.split("\n");
+			curr = calculateValue(splitLines[1], currDelimiter);
+		}
+		return curr;
 	}
 }
--- OfflineAssesment/src/test/java/com/java/assignment/tests/TestStringNumbers.java
+++ OfflineAssesment/src/test/java/com/java/assignment/tests/TestStringNumbers.java
@@ -13,9 +13,10 @@ public class TestStringNumbers {
 		System.out.println(" Test 5, String is: 1,,3 the result is:" + obj.add("1,,3"));
 		System.out.println(" Test 6, String is: 1, the result is:" + obj.add("1,"));
 		System.out.println(" Test 7, String is: ,3 the result is:" + obj.add(",3"));
-		System.out.println(" Test 8, String is: 1\\n2,3 the result is:" + obj.add("1\\n2,3"));
-		System.out.println(" Test 9, String is: 1\\n2,3\\n4,5,6\\n7,9 the result is:" + obj.add("1\\n2,3\\n4,5,6\\n7,9"));
-		System.out.println(" Test 10,String is: 10\\n,3\\n5,6 the result is:" + obj.add("10\\n,3\\n5,6"));
-		System.out.println(" Test 11,String is: 10\\n,3\\n5,\\n the result is:" + obj.add("10\\n,3\\n5,\\n"));
+		System.out.println(" Test 8, String is: 1\\n2,3 the result is:" + obj.add("1\n2,3"));
+		System.out.println(" Test 9, String is: 1\\n2,3\\n4,5,6\\n7,9 the result is:" + obj.add("1\n2,3\n4,5,6\n7,9"));
+		System.out.println(" Test 10,String is: 10\\n,3\\n5,6 the result is:" + obj.add("10\n,3\n5,6"));
+		System.out.println(" Test 11,String is: 10\\n,3\\n5,\\n the result is:" + obj.add("10\n,3\n5,\n"));
+		System.out.println(" Test 12,String is: //;\\n1;2 the result is:" + obj.add("//;\n1;2"));
 	}
 }
